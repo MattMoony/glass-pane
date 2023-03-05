1dba6f35e3f0c28c05dea16b6064eba18201edad
Matthias Monschein <m4ttm00ny@gmail.com>
2023-03-05 17:39:42 +0100

    :art: better indentation

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,16 +2,16 @@
 </script>
 
 <template>
-    <div>
-        <img src="/pane.png" alt="pane" />
-        <h1>Glass Pane</h1>
-    </div>
+  <div>
+    <img src="/pane.png" alt="pane" />
+    <h1>Glass Pane</h1>
+  </div>
 </template>
 
 <style scoped>
 h1 {
-    user-select: none;
-    font-family: 'Comfortaa', sans-serif;
-    font-size: 3rem;
+  user-select: none;
+  font-family: 'Comfortaa', sans-serif;
+  font-size: 3rem;
 }
 </style>
--- src/views/Person.vue
+++ src/views/Person.vue
@@ -5,79 +5,79 @@ import PersonNetwork from '../components/PersonNetwork.vue'
 </script>
 
 <template>
-    <article>
-        <NavBar :qry="this.$route.params.id" />
-        <div class="person-container">
-            <div class="part-headings">
-                <div>
-                    <span>
-                        <font-awesome-icon icon="fa-solid fa-eye-slash" />
-                        Description
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        <font-awesome-icon icon="fa-solid fa-eye-slash" />
-                        Network
-                    </span>
-                </div>
-            </div>
-            <div class="details">
-                <div class="person-desc">
-                    <PersonDescription />
-                </div>
-                <div class="person-netw">
-                    <PersonNetwork />
-                </div>
-            </div>
+  <article>
+    <NavBar :qry="this.$route.params.id" />
+    <div class="person-container">
+      <div class="part-headings">
+        <div>
+          <span>
+            <font-awesome-icon icon="fa-solid fa-eye-slash" />
+            Description
+          </span>
         </div>
-    </article>
+        <div>
+          <span>
+            <font-awesome-icon icon="fa-solid fa-eye-slash" />
+            Network
+          </span>
+        </div>
+      </div>
+      <div class="details">
+        <div class="person-desc">
+          <PersonDescription />
+        </div>
+        <div class="person-netw">
+          <PersonNetwork />
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <style scoped>
 article {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: stretch;
-    align-items: stretch;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  align-items: stretch;
 }
 
 .person-container {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: stretch;
-    align-items: stretch
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  align-items: stretch
 }
 
 .part-headings {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 1fr;
-    border-bottom: 2px solid var(--color-border);
-    user-select: none;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr;
+  border-bottom: 2px solid var(--color-border);
+  user-select: none;
 }
 
 .part-headings div span {
-    display: inline-block;
-    cursor: pointer;
-    padding: .2em;
-    font-weight: bold;
+  display: inline-block;
+  cursor: pointer;
+  padding: .2em;
+  font-weight: bold;
 }
 
 .details {
-    flex: 1;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
+  flex: 1;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 }
 
 .person-desc {
-    padding: 2em;
+  padding: 2em;
 }
 
 .person-netw {
-    overflow: hidden;
-    border-left: 2px solid var(--color-border);
+  overflow: hidden;
+  border-left: 2px solid var(--color-border);
 }
 </style>
