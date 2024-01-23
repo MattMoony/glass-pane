707776e7ce734af7b4be7518e0e2c95595100bd0
Matthias Monschein <monschein.matthias@gmail.com>
2024-01-23 21:20:55 +0100

    :sparkles: new start

--- core/src/db.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg';
-
-export const pool: Pool = new Pool({
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
--- core/src/routes/person.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controllers/person';
-
-const router: Router = Router();
-
-router.post('/new', controller.create);
-router.get('/:userId', controller.get);
-
-export default router;
